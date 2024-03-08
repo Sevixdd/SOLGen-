a1af7a647e6176b75393dbae8aebd5fd81b92873
Sebastian Dan <sebi.momir@gmail.com>
2024-03-08 20:30:54 +0000

    change

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import streamlit as st
 import asyncio
+import scipy
+
+from transformers import AutoTokenizer, AutoModelForTextToWaveform
 
 async def generate_text_simulated(prompt, delay=1):
     # Simulating text generation by yielding chunks over time
@@ -12,6 +15,27 @@ def main():
 
     prompt = st.text_input("Enter your prompt:", "Hello")
 
+
+    tokenizer = AutoTokenizer.from_pretrained("facebook/musicgen-medium")
+    model = AutoModelForTextToWaveform.from_pretrained("facebook/musicgen-medium")
+    inputs = processor(
+    text=["80s pop track with bassy drums and synth", "90s rock song with loud guitars and heavy drums"],
+    padding=True,
+    return_tensors="pt",
+    )
+    
+    pad_token_id = model.generation_config.pad_token_id
+    decoder_input_ids = (
+    torch.ones((inputs.input_ids.shape[0] * model.decoder.num_codebooks, 1), dtype=torch.long)
+    * pad_token_id
+    )
+
+    logits = model(**inputs, decoder_input_ids=decoder_input_ids).logits
+    logits.shape  # (bsz * num_codebooks, tgt_len, vocab_size)
+
+
+    sampling_rate = model.config.audio_encoder.sampling_rate
+    scipy.io.wavfile.write("musicgen_out.wav", rate=sampling_rate, data=audio_values[0, 0].numpy())
     if st.button("Generate"):
         text_placeholder = st.empty()
         text_placeholder.text("Generating response...")
@@ -33,4 +57,4 @@ def main():
         threading.Thread(target=run_async).start()
 
 if __name__ == "__main__":
-    main()
+    main()  
